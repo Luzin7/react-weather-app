5acb4b08cd00c99e0bd6f50970c7440e3c0a0cf8
Luan Victor <lvictor.dutra@gmail.com>
2022-05-10 20:14:51 -0300

    Create FavoriteWeathers.jsx

---
+++ src/components/FavoriteWeathers.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class FavoriteWeathers extends Component {
+  render () {
+    return (
+      <div>FavoriteWeathers</div>
+    );
+  }
+}
