530a29d8e2b474f56662d7c7af2222e9fe7b0905
Luan Victor <lvictor.dutra@gmail.com>
2022-05-13 12:05:07 -0300

    atualiza componente

--- src/components/FavoriteWeathers.jsx
+++ src/components/FavoriteWeathers.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class FavoriteWeathers extends Component {
   render () {
+    const { favoriteLocal } = this.props;
     return (
-      <div>FavoriteWeathers</div>
+      <div>
+        {favoriteLocal.map((favorite) => (
+          <h1 key={favorite.id}>{favorite.name}</h1>
+        ))}
+      </div>
     );
   }
 }
+
+FavoriteWeathers.propTypes = {
+  favoriteLocal: PropTypes.arrayOf(Object).isRequired
+};
