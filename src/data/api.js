cc9be7ed9f5f7bb0b9418861eb28d5d17cd89c11
Luan Victor <lvictor.dutra@gmail.com>
2022-05-15 05:03:36 -0300

    ♻️refactor: Tira linhas inúteis

--- src/data/api.js
+++ src/data/api.js
@@ -4,10 +4,6 @@ const fetchWeather = async (city) => {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=973db35b274b2d5eaa4ba88d11c72dee&units=metric`
     );
     const data = await fetchRequest.json();
-    // console.log(data.name);
-    // console.log(data.weather[0].main);
-    console.log(data.weather[0].description);
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error.message);
