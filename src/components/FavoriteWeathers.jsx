ebda635bc2f4d323b359eb4cf18a06fc618d839b
Luan Victor <lvictor.dutra@gmail.com>
2022-05-17 23:51:17 -0300

    🚧

--- src/components/FavoriteWeathers.jsx
+++ src/components/FavoriteWeathers.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../style/favoriteWeathers/favoriteBox.css';
+import '../style/favoriteWeathers/container.css';
 
 export default class FavoriteWeathers extends Component {
+  // Chama func com o metodo pra deletar o fav
+  removeFav (fav) {
+    this.props.removeFav(fav);
+  }
+
   render () {
     const { favoriteLocal } = this.props;
     return (
-      <div className="container">
+      <div className="container__fav">
         {favoriteLocal.map((favorite) => (
-          <div key={favorite.id} className="favorite__box">
+          // Ao clicar a funcao é chamada por uma arrow function
+          <div onClick={() => { this.removeFav(favorite); } } key={favorite.id} className="info__box" id="favorite__box">
             <div className="locale">
-              <h1>{favorite.name} <span>{favorite.sys.country}</span></h1>
+              <h1>
+                {favorite.name} <span>{favorite.sys.country}</span>
+              </h1>
             </div>
             <div className="temp__Info">
               <h2>{`${favorite.main.temp.toFixed(0)}°c`}</h2>
@@ -25,5 +35,6 @@ export default class FavoriteWeathers extends Component {
 }
 
 FavoriteWeathers.propTypes = {
-  favoriteLocal: PropTypes.arrayOf(Object).isRequired
+  favoriteLocal: PropTypes.arrayOf(Object).isRequired,
+  removeFav: PropTypes.func.isRequired
 };
