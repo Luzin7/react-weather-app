d9fa8ce25917db6c020a5480bb27c0fcc7d22b90
Luan Victor <lvictor.dutra@gmail.com>
2022-05-21 20:45:51 -0300

    ♻️refactor: Add link
    Add link para o meu perfil no GitHub.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import '../style/header.css';
+import gitLogo from '../assets/img/GitHub-Mark-Light-32px.png';
 
 export default class Header extends Component {
+  // tempoAtual = () => {
+  //   setInterval(timer, 1000);
+
+  //   function timer () {
+  //     const date = new Date();
+  //     const hours = date.getHours();
+  //     const minutes = date.getMinutes();
+  //     const seconds = date.getSeconds();
+  //     const timer = document.querySelector('.timer');
+
+  //     if (seconds < 10) {
+  //       timer.textContent = `${hours}:${minutes}:0${seconds}`;
+  //     } else if (minutes < 10) {
+  //       timer.textContent = `${hours}:0${minutes}:${seconds}`;
+  //     } else timer.textContent = `${hours}:${minutes}:${seconds}`;
+  //   };
+  // };
+
   render () {
     return (
       <div className='header'>
+        <a className='gitLogo' href="https://github.com/Luzin7" target="_blank" rel="noreferrer noopener"><img src={gitLogo} alt="Github Logo" /></a>
         <h1>Weather App</h1>
       </div>
     );
