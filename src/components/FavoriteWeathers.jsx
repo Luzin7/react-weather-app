2649b5eec6e8e0100676ec1a897fc91626ed8049
Luan Victor <lvictor.dutra@gmail.com>
2022-05-22 15:42:46 -0300

    💫feat: Add animações nos componentes

--- src/components/FavoriteWeathers.jsx
+++ src/components/FavoriteWeathers.jsx
@@ -7,18 +7,7 @@ export default class FavoriteWeathers extends Component {
   // Chama func com o metodo pra deletar o fav
   removeFav (fav) {
     this.props.removeFav(fav);
-  };
-
-  tempoAtual = () => {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const day = date.getDate();
-    const month = date.getMonth();
-    const year = date.getFullYear();
-
-    return `Saved at ${hours}:${minutes}, ${day}/${month}/${year}`;
-  };
+  }
 
   render () {
     const { favoriteLocal } = this.props;
@@ -33,19 +22,19 @@ export default class FavoriteWeathers extends Component {
             key={favorite.id}
             className="info__box"
             id="favorite__box"
+            data-aos="fade-up"
           >
-            <span className='dateSaved'>{this.tempoAtual()}</span>
             <div className="locale">
-
               <h1>
                 {favorite.name},<span> {favorite.sys.country}</span>
               </h1>
             </div>
-            <div className="temp__Info">
-              <h2>{`${favorite.main.temp.toFixed(0)}°c`}</h2>
-            </div>
             <div className="desc">
               <p>{favorite.weather[0].description}</p>
+            </div>
+            <span className="savedTime">{favorite.savedTime}</span>
+            <div className="temp__Info">
+              <h2>{`${favorite.main.temp.toFixed(0)}°c`}</h2>
             </div>
           </div>
         ))}
