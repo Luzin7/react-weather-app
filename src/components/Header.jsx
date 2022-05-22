6abec0b7ea3064dc1b0d93228215423f916aa082
Luan Victor <lvictor.dutra@gmail.com>
2022-05-22 16:46:21 -0300

    ♻️refactor: Add autoria

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,10 +22,13 @@ export default class Header extends Component {
   // };
 
   render () {
+    const date = new Date();
+    const year = date.getFullYear();
     return (
       <div className='header'>
         <a data-aos="zoom-in" className='gitLogo' href="https://github.com/Luzin7" target="_blank" rel="noreferrer noopener"><img src={gitLogo} alt="Github Logo" /></a>
         <h1>Weather App</h1>
+        <span>&copy; {year} Luan Victor</span>
       </div>
     );
   }
